885da7aa8917dd6f62bdf41f6da6c814cf889ca2
DinDin03 <dineth.himasha@outlook.com>
2025-06-27 16:28:58 +0930

    password reset function started

---
+++ src/main/java/com/friendavailability/model/PasswordResetToken.java
@@ -0,0 +1,13 @@
+package com.friendavailability.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+public class PasswordResetToken {
+
+}
