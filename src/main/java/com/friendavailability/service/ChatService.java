176b7f22ce711e6aaad860121add23eef36cad14
DinDin03 <dineth.himasha@outlook.com>
2025-07-10 12:26:51 +0930

    ChatService

---
+++ src/main/java/com/friendavailability/service/ChatService.java
@@ -0,0 +1,13 @@
+package com.friendavailability.service;
+
+import com.friendavailability.model.*;
+import com.friendavailability.repository.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
