15cf86ff73e694435bb26bcde0c4e5f537a742f0
DinDin03 <126632307+DinDin03@users.noreply.github.com>
2025-07-16 03:16:12 +0000

    Circle started

--- src/main/java/com/friendavailability/controller/AuthController.java
+++ src/main/java/com/friendavailability/controller/AuthController.java
@@ -28,7 +28,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = {"http://localhost:8080", "http://127.0.0.1:8080", "https://friendavailability-production.up.railway.app", "https://www.dineth.au"})
+@CrossOrigin(origins = {"http://localhost:8080", "http://127.0.0.1:8080", "https://friendavailability-production.up.railway.app", "https://www.linkups.com.au"})
 public class AuthController {
 
     @Autowired
