9cabe20309396e0854f6f00cdf0f8cd84bc47523
DinDin03 <126632307+DinDin03@users.noreply.github.com>
2025-07-16 03:24:32 +0000

    Circle started

---
+++ src/main/java/com/friendavailability/model/Circle.java
@@ -0,0 +1,14 @@
+package com.friendavailability.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+
