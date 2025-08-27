d41f1ed954714c5b8a4b3893aeb2c8bdf0f5457c
DinDin03 <dineth.himasha@outlook.com>
2025-08-27 19:54:20 +0930

    LINK-4 feat(exception): implemented DuplicateResourceException for the backend global exception handler

--- backend/src/main/java/com/friendavailability/domain/exception/DuplicateResourceException.java
+++ backend/src/main/java/com/friendavailability/domain/exception/DuplicateResourceException.java
@@ -1,7 +1,27 @@
 package com.friendavailability.domain.exception;
 
 import org.springframework.http.HttpStatus;
-//testing jira
-public class DuplicateResourceException extends RuntimeException{
+
+public class DuplicateResourceException extends BusinessException{
+    public DuplicateResourceException(String resourceName, String field, String value){
+        super(
+                String.format("%s with %s '%s' already exists", resourceName, field, value),
+                "DUPLICATE_RESOURCE",
+                HttpStatus.CONFLICT
+        );
+        withDetail("resource", resourceName);
+        withDetail("field", field);
+        withDetail("value", value);
+    }
+
+    public static DuplicateResourceException duplicateEmail(String email){
+        return new DuplicateResourceException("User", "email", email);
+    }
+
+    public static DuplicateResourceException duplicateCircleName(String circleName){
+        return new DuplicateResourceException("Circle", "name", circleName);
+    }
+
+    // I will create more duplicate exceptions in the future
 
 }
