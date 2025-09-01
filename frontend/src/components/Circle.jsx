f2cc23d8cdc619ff5e8a59ab416ff40a685321cd
Leo <leo.nguyen@student.adelaide.edu.au>
2025-09-01 14:39:19 +0930

    added functionality to some of the buttons, mainly signup

--- frontend/src/components/Circle.jsx
+++ frontend/src/components/Circle.jsx
@@ -1,23 +1,59 @@
+import {cn} from '@/lib/utils'
+import { useState, useEffect } from 'react';
+import { SignupForm } from '@/components/SignupForm.jsx';
+import { authService } from '@/services/authService';
+
 import friendsCircle from "@/assets/Circles/circles-1.png"
 import friendsGroup from "@/assets/Circles/friends-expanded-1.png"
 import friendsChat from "@/assets/Circles/friends-chat-1.png"
 
 export const Circle = () => {
+    const [isSignUpOpen, setIsSignUpOpen] = useState(false);
+
+    useEffect(() => {
+        if (isSignUpOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    }, [isSignUpOpen]);
+
     return (
-        <section id="circle" className="relative p-12 flex flex-col items-center justify-center">
-            <div className="bg-zinc-800 rounded-full size-200 p-30 flex flex-col items-center justify-start">
-                <h1 className="text-3xl font-bold text-white">All your friends, one circle</h1>
-                    <p className="text-white m-10 w-80">
-                        Create or join circles where friends can make plans and chat together on the go.
-                    </p>
-                    <button className="button">Get started</button>
-                    <div className="flex flex-row items-center justify-center gap-8">
-                         <img src={friendsCircle} alt="circle" className="h-93 w-90 object-contain card-hover"/>
-                         <img src={friendsGroup} alt="circle" className="h-100 w-90 object-contain card-hover"/>
-                         <img src={friendsChat} alt="circle" className="h-100 w-93 object-contain card-hover"/>
-                     </div>
+        <>
+            {/* Sign up module */}
+            <div 
+                className={cn(
+                    "fixed inset-0 bg-gray-500/50 backdrop-blur-md z-[60] flex flex-col items-center justify-center",
+                    "transition-all duration-300",
+                    isSignUpOpen 
+                        ? "opacity-100 pointer-events-auto" 
+                        : "opacity-0 pointer-events-none"
+                )}
+                onClick={() => setIsSignUpOpen(false)}
+            >
+                <SignupForm onClose={() => setIsSignUpOpen(false)}/>
             </div>
-        </section>
+            
+            <section id="circle" className="relative p-12 flex flex-col items-center justify-center">
+                <div className="bg-zinc-800 rounded-full size-200 p-30 flex flex-col items-center justify-start">
+                    <h1 className="text-3xl font-bold text-white">All your friends, one circle</h1>
+                        <p className="text-white m-10 w-80">
+                            Create or join circles where friends can make plans and chat together on the go.
+                        </p>
+                        <button className="button" onClick={() => setIsSignUpOpen((prev) => !prev)}>Get started</button>
+                        <div className="flex flex-row items-center justify-center gap-8">
+                            <img src={friendsCircle} alt="circle" className="h-93 w-90 object-contain card-hover"/>
+                            <img src={friendsGroup} alt="circle" className="h-100 w-90 object-contain card-hover"/>
+                            <img src={friendsChat} alt="circle" className="h-100 w-93 object-contain card-hover"/>
+                        </div>
+                </div>
+            </section>
+        </>
+        
     )
     
 }
--- frontend/src/components/Works.jsx
+++ frontend/src/components/Works.jsx
@@ -1,3 +1,7 @@
+import {cn} from '@/lib/utils'
+import { useState, useEffect } from 'react';
+import { SignupForm } from '@/components/SignupForm.jsx';
+
 import calendar1 from "@/assets/Calendar-amico/Calendar-amico-1.png"
 import calendar2 from "@/assets/Calendar-amico/Calendar-amico-2.png"
 import calendar3 from "@/assets/Calendar-amico/Calendar-amico-3.png"
@@ -6,6 +10,20 @@ import CalendarImage from '@/assets/friends-calendar.png'
 import { Mouse, MousePointer } from 'lucide-react'
 
 export const Works = () => {
+
+    const [isSignUpOpen, setIsSignUpOpen] = useState(false);
+
+    useEffect(() => {
+        if (isSignUpOpen) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'unset';
+        }
+
+        return () => {
+            document.body.style.overflow = 'unset';
+        };
+    }, [isSignUpOpen]);
 
     const cards = [
         {
@@ -28,59 +46,79 @@ export const Works = () => {
         },
     ]
     return (
-        <section id="works" className="relative pt-36 flex flex-col items-center justify-center">
-            <div className="container">
-                <h1 className="text-3xl font-bold">How it works</h1>
-                <p className="text-lg mt-4 ">Get from chaos to coordinated in three simple steps.</p>
+        <>
+            {/* Sign up module */}
+            <div 
+                className={cn(
+                    "fixed inset-0 bg-gray-500/50 backdrop-blur-md z-[60] flex flex-col items-center justify-center",
+                    "transition-all duration-300",
+                    isSignUpOpen 
+                        ? "opacity-100 pointer-events-auto" 
+                        : "opacity-0 pointer-events-none"
+                )}
+                onClick={() => setIsSignUpOpen(false)}
+            >
+                <SignupForm onClose={() => setIsSignUpOpen(false)}/>
             </div>
 
-            <div className="grid grid-cols-3 gap-12 pt-10 px-60">
-                {cards.map((card, key) => (
-                    <div
-                        key={key}
-                        className="group border border-gray-200 rounded-[60px] overflow-hidden card-hover shadow-xs p-8 w-80"    
-                    >
-                        <img 
-                            src={card.img} 
-                            alt="card image" 
-                            className="w-full h-60 object-cover my-10" 
-                        />
-                        <h1 className="text-xl font-semibold mt-1">{card.title}</h1>
-                        <p className="mt-6 mb-16">{card.description}</p>
-                    </div>
-                ))}
-            </div>
+            <section id="works" className="relative pt-36 flex flex-col items-center justify-center">
+                <div className="container">
+                    <h1 className="text-3xl font-bold">How it works</h1>
+                    <p className="text-lg mt-4 ">Get from chaos to coordinated in three simple steps.</p>
+                </div>
 
-            <div className="mt-36 flex flex-row rounded-[100px] bg-primary p-12 h-full w-[1250px]">
-                <div className='flex-1'>
-                    <div className='bg-white size-26 rounded-full flex items-center justify-center'>
-                        <img src={Logo} className='size-20'/>
-                    </div> 
-                    <div className='flex flex-col items-start justify-start pl-25 pt-20'>
-                        <h1 
-                            className="text-3xl text-background font-bold flex flex-row items-center justify-center"
+                <div className="grid grid-cols-3 gap-12 pt-10 px-60">
+                    {cards.map((card, key) => (
+                        <div
+                            key={key}
+                            className="group border border-gray-200 rounded-[60px] overflow-hidden card-hover shadow-xs p-8 w-80"    
                         >
-                            Click, view, plan <MousePointer className='ml-4 size-7'/>
-                        </h1>
-                        <p className='text-background mt-6 text-left'>
-                            With our interactive calendar, you can view everyone's availability by simply clicking on the date.
-                        </p>
-                        <div className='flex flex-row items-center justify-start mt-10 gap-6'>
-                            <button className='bg-white font-bold button text-primary'>
-                                Get started
-                            </button>
-                            <button className='text-background'>
-                                Learn more
-                            </button>
+                            <img 
+                                src={card.img} 
+                                alt="card image" 
+                                className="w-full h-60 object-cover my-10" 
+                            />
+                            <h1 className="text-xl font-semibold mt-1">{card.title}</h1>
+                            <p className="mt-6 mb-16">{card.description}</p>
                         </div>
-                        
-                    </div>
-                </div>    
-                <div className='p-10 grid grid-cols-1 grid-rows-1 card-hover'>
-                    <img src={CalendarImage} className='z-10 w-full h-100 object-contain col-start-1 row-start-1'/>
-                    <img src={CalendarImage} className='w-full h-100 object-contain col-start-1 row-start-1 rotate-10 translate-x-10'/>
-                </div> 
-            </div>
-        </section>
+                    ))}
+                </div>
+
+                <div className="mt-36 flex flex-row rounded-[100px] bg-primary p-12 h-full w-[1250px]">
+                    <div className='flex-1'>
+                        <div className='bg-white size-26 rounded-full flex items-center justify-center'>
+                            <img src={Logo} className='size-20'/>
+                        </div> 
+                        <div className='flex flex-col items-start justify-start pl-25 pt-20'>
+                            <h1 
+                                className="text-3xl text-background font-bold flex flex-row items-center justify-center"
+                            >
+                                Click, view, plan <MousePointer className='ml-4 size-7'/>
+                            </h1>
+                            <p className='text-background mt-6 text-left'>
+                                With our interactive calendar, you can view everyone's availability by simply clicking on the date.
+                            </p>
+                            <div className='flex flex-row items-center justify-start mt-10 gap-6'>
+                                <button 
+                                    className='bg-white font-bold button text-primary'
+                                    onClick={() => setIsSignUpOpen((prev) => !prev)}
+                                >
+                                    Get started
+                                </button>
+                                <button className='text-background'>
+                                    Learn more
+                                </button>
+                            </div>
+                            
+                        </div>
+                    </div>    
+                    <div className='p-10 grid grid-cols-1 grid-rows-1 card-hover'>
+                        <img src={CalendarImage} className='z-10 w-full h-100 object-contain col-start-1 row-start-1'/>
+                        <img src={CalendarImage} className='w-full h-100 object-contain col-start-1 row-start-1 rotate-10 translate-x-10'/>
+                    </div> 
+                </div>
+            </section>
+        </>
+        
     )
 }
