69241a78daad4ea7ce53217d4247fd5cd1d0fc09
DinDin03 <126632307+DinDin03@users.noreply.github.com>
2025-09-05 02:00:15 +0000

    insufficient permission exception handler implemented with two convienience methods. one for not circle owner and other for not friends exceptions. more methods will be implemented in the future

--- backend/src/main/java/com/friendavailability/domain/exception/InsufficientPermissionException.java
+++ backend/src/main/java/com/friendavailability/domain/exception/InsufficientPermissionException.java
@@ -1,5 +1,31 @@
 package com.friendavailability.domain.exception;
 
-public class InsufficientPermissionException {
+import org.springframework.http.HttpStatus;
+
+public class InsufficientPermissionException extends BusinessException {
     
+    public InsufficientPermissionException(String action, String resource) {
+        super(
+            String.format("You don't have permission to %s this %s", action, resource),
+            "INSUFFICIENT_PERMISSION",
+            HttpStatus.FORBIDDEN  
+        );
+        withDetail("action", action);
+        withDetail("resource", resource);
+    }
+    
+    public InsufficientPermissionException(String message) {
+        super(message, "INSUFFICIENT_PERMISSION", HttpStatus.FORBIDDEN);
+    }
+
+    public static InsufficientPermissionException notCircleOwner(Long circleId) {
+        return new InsufficientPermissionException("Only the circle owner can perform this action")
+            .withDetail("circleId", circleId);
+    }
+
+    public static InsufficientPermissionException notFriends(Long userId1, Long userId2) {
+        return new InsufficientPermissionException("Users must be friends to perform this action")
+            .withDetail("userId1", userId1)
+            .withDetail("userId2", userId2);
+    }
 }
