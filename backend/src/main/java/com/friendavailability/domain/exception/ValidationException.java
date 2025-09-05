e94eccfd4d05c47892dcc51d058cbb182443c912
DinDin03 <126632307+DinDin03@users.noreply.github.com>
2025-09-05 01:43:31 +0000

    validation exception class completed with two specific Validation method
    - invalid date range exception with detailed error handling
    - invalid password exception with also detailed error handling
    
    more specific error handling cases will be implemented in the future

--- backend/src/main/java/com/friendavailability/domain/exception/ValidationException.java
+++ backend/src/main/java/com/friendavailability/domain/exception/ValidationException.java
@@ -1,5 +1,39 @@
 package com.friendavailability.domain.exception;
 
-public class ValidationException {
+import org.springframework.http.HttpStatus;
+import java.util.Map;
+import java.util.HashMap;
+
+import java.time.LocalDateTime;
+
+public class ValidationException extends BusinessException{
     
+    public ValidationException(String message){
+        super(
+            message,
+            "VALIDATION_FAILED",
+            HttpStatus.BAD_REQUEST
+        );
+    }
+
+    public ValidationException(String field, String message) {
+        super(
+            message,
+            "VALIDATION_FAILED",
+            HttpStatus.BAD_REQUEST
+        );
+        withDetail("field", field);
+    }
+
+    public static ValidationException invalidDateRange(LocalDateTime start, LocalDateTime end){
+        return new ValidationException("Start time must be before end time")
+        .withDetail("startTime", start.toString())
+        .withDetail("endTime", end.toString())
+    }
+
+    public static ValidationException invalidPassword(String reason) {
+    return new ValidationException("password", "Invalid password: " + reason);
+}
+
+
 }
