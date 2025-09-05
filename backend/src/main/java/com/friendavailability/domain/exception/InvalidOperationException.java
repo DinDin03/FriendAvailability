cc328734cbfcb1b4ae24d153c7d9173b33f2ae2b
DinDin03 <126632307+DinDin03@users.noreply.github.com>
2025-09-05 02:20:45 +0000

    last exception class completed. Invalid operation exception implemented with three convienience methods and more will be implemented in the future

--- backend/src/main/java/com/friendavailability/domain/exception/InvalidOperationException.java
+++ backend/src/main/java/com/friendavailability/domain/exception/InvalidOperationException.java
@@ -1,5 +1,28 @@
 package com.friendavailability.domain.exception;
+import org.springframework.http.HttpStatus;
 
-public class InvalidOperationException {
-    
+public class InvalidOperationException extends BusinessException {
+    public InvalidOperationException(String message){
+        super(
+            message,
+            "INVALID_OPERATION"
+            HttpStatus.BAD_REQUEST
+        )
+    }
+
+    public static InvalidOperationException cannotAddSelfAsFriend() {
+        return new InvalidOperationException("You cannot add yourself as a friend");
+    }
+
+    public static InvalidOperationException circleAtCapacity(Long circleId, int maxMembers) {
+        return new InvalidOperationException("Circle has reached maximum capacity")
+            .withDetail("circleId", circleId)
+            .withDetail("maxMembers", maxMembers);
+    }
+
+    public static InvalidOperationException alreadyMember(Long userId, Long circleId) {
+        return new InvalidOperationException("User is already a member of this circle")
+            .withDetail("userId", userId)
+            .withDetail("circleId", circleId);
+    }
 }
