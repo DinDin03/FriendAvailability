7104ccf398127467160c8d6459536f7ab1a19b77
DinDin03 <dineth.himasha@outlook.com>
2025-09-23 19:41:27 +0930

    Circle Controller updated

---
+++ backend/src/main/java/com/friendavailability/api/dto/response/circle/CircleMemberListResponse.java
@@ -0,0 +1,16 @@
+package com.friendavailability.api.dto.response.circle;
+import lombok.Data;
+import lombok.Builder;
+import java.util.List;
+
+@Data
+@Builder
+public class CircleMemberListResponse {
+
+    private List<CircleMemberResponse> members;
+    private Integer totalMembers;
+
+    private Integer ownerCount;
+    private Integer adminCount;
+    private Integer memberCount;
+}
